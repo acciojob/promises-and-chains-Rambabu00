753e1e61a47a4312a4afb29060911b06859b15b0
Rambabu00 <123948696+Rambabu00@users.noreply.github.com>
2023-04-11 16:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+function submit (){
+	var age=document.getElementById("age").value;
+	var name =document.getElementById("name").value;
+	if(age=="" || name==""){
+		alert("inputs cannot be empty");
+	}
+	else{
+		return new Promise((resolve, reject)=>{
+			setTimeout(()=>{
+				if(age>18){
+					resolve("Welcome, "+name+". You can vote");
+				}
+				else{
+					reject("Oh sorry "+name+". You aren't old enough");
+				}
+			},4000);
+		}).then((data)=>{
+			alert(data);
+})
+		.catch((err)=>{
+			alert(err);
+		})
+	}
+}
